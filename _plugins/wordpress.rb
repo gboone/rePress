cb9872f4903e10fa706a7337d10e37fdcbefeb35
gboone <boone.greg@gmail.com>
2015-01-29 07:47:30 -0500

    ignores the config.yml, adjusts for missing tags

--- _plugins/wordpress.rb
+++ _plugins/wordpress.rb
@@ -20,35 +20,37 @@ module Jekyll
 
 		def grabPosts(site)
 			index = WpPost.new(site, site.source)
-			data = open("#{site.config['wpApi']}posts/")
+			puts("WordPress API: #{site.config['wpApi']}")
+			data = open(site.config['wpApi'] + "posts/")
 			wpposts = JSON.load(data)
 		end
 
 		def generatePosts(site, data)
-			# require 'pry'; binding.pry
 			for d in data
 				puts("Building post #{d['title']}")
-				# post_date = Date.parse(d['date']).strftime("%Y-%m-%d")
-				# filename = "#{post_date}-#{d['slug']}.html"
-				# path = File.join(site.source, "_posts", filename)
-				# tags = []
-				# for t in d['terms']['post_tag']
-				# 	name = t['name'].strip
-				# 	tags.push(name)
-				# end
-				# frontmatter = {}
-				# frontmatter[:title] = d['title']
-				# frontmatter[:author] = d['author']['username']
-				# frontmatter[:excerpt] = d['excerpt'].gsub(%r{</?[^>]+?>}, '').strip
-				# frontmatter[:tags] = tags
-				# frontmatter[:layout] = d['format']
-				# content = d['content'].strip
+				post_date = Date.parse(d['date']).strftime("%Y-%m-%d")
+				filename = "#{post_date}-#{d['slug']}.html"
+				path = File.join(site.source, "_posts", filename)
+				tags = []
+				unless d['terms']['post_tag'].nil? 
+					for t in d['terms']['post_tag'] 
+						name = t['name'].strip
+						tags.push(name)
+					end
+				end
+				frontmatter = {}
+				frontmatter[:title] = d['title']
+				frontmatter[:author] = d['author']['username']
+				frontmatter[:excerpt] = d['excerpt'].gsub(%r{</?[^>]+?>}, '').strip
+				frontmatter[:tags] = tags
+				frontmatter[:layout] = d['format']
+				content = d['content'].strip
 
-				# front = YAML.load(frontmatter.to_json)
-				# front_yaml = YAML.dump(front)
-				# contents = YAML.load(content.to_json)
-				# contents_yaml = YAML.dump(contents)
-				# File.open(path, 'w') { |file| file.write(front_yaml + contents_yaml)}
+				front = YAML.load(frontmatter.to_json)
+				front_yaml = YAML.dump(front)
+				contents = YAML.load(content.to_json)
+				contents_yaml = YAML.dump(contents)
+				File.open(path, 'w') { |file| file.write(front_yaml + contents_yaml)}
 				puts("Updated or created #{filename}")
 			end
 		end
